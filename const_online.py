815f278b90c32313fef6b8002f30c865a24fef20
Shinzx08 <89729321+Shinzx08@users.noreply.github.com>
2025-03-29 19:06:36 +0800

    Update const_online.py

--- const_online.py
+++ const_online.py
@@ -1,15 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask
 from threading import Thread
+from dotenv import load_dotenv
+import os
 
+# Initialize the Flask app
 app = Flask(__name__)
+
+load_dotenv()
 
 @app.route('/')
 def index():
     return "Alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    port = int(os.getenv("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
-def const_online():  
+def const_online():
     t = Thread(target=run)
     t.start()
+
+if __name__ == '__main__':
+    const_online()
